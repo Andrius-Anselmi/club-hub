1ef061591c00eb5e00c8d52e1346f887452fd96d
andrius anselmi <andriusanselmi12@gmail.com>
2026-07-22 14:43:42 -0300

    chore: configuração inicial do projeto

---
+++ src/test/java/io/github/andrius_anselmi/club_hub/ClubHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrius_anselmi.club_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
